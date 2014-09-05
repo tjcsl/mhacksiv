527c414da01dd40425086253dec2007c54e30675
Vacation9 <samuel.damashek@gmail.com>
2014-09-05 04:01:41 -0700

    Update API keys and phone number

--- send_reminders.py
+++ send_reminders.py
@@ -1,12 +1,12 @@
 from twilio.rest import TwilioRestClient
 import project.utils.reminders
 
-ACCOUNT_SID = "AC6a9746370384b26236aae71013aa35b2"
-AUTH_TOKEN = "38b0bcc37788e553978c840929d54aa2"
+ACCOUNT_SID = "ayylmao"
+AUTH_TOKEN = "ayylmao"
 
 def send_reminder(text, phone):
     client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN)
-    client.messages.create(to=phone, from_="+15713646776", body=text)
+    client.messages.create(to=phone, from_="+15172194225", body=text)
 
 def send_all_reminders():
     x = project.utils.reminders.get_needed_reminders()
