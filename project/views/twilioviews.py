877e5f6c4613c1ce4f7e047fd392f79b656dbf10
Vacation9 <samuel.damashek@gmail.com>
2014-09-05 04:56:39 -0700

    More debugging

--- project/views/twilioviews.py
+++ project/views/twilioviews.py
@@ -21,10 +21,13 @@ def text():
     if intent == 'get_status':
         m = get_status(wit, phone)
     elif intent == 'remind':
-        entities = json.loads(wit)['outcomes'][0]['entities']
-        date = dateutil.parser.parse(entities['time'][0]['value']['from'])
-        text = entities['message'][0]['value']
-        m = create_reminder(date, text, phone)
+        try:
+            entities = json.loads(wit)['outcomes'][0]['entities']
+            date = dateutil.parser.parse(entities['time'][0]['value']['from'])
+            text = entities['message'][0]['value']
+            m = create_reminder(date, text, phone)
+        except Exception, e:
+            print str(e)
     else:
         m = "Hmm? Try again please :("
 
