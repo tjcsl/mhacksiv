95e4a301fe28b0c937a256062a0563689018643d
Vacation9 <samuel.damashek@gmail.com>
2014-09-05 03:59:37 -0700

    Updated twilio views for remind

--- project/views/twilioviews.py
+++ project/views/twilioviews.py
@@ -1,7 +1,10 @@
 from flask import request
 import requests
 from ..utils.status import get_status
+from ..utils.reminders import create_reminder
 import twilio.twiml
+import json
+from datetime import datetime
 
 def call():
     resp = twilio.twiml.Response()
@@ -13,6 +16,17 @@ def text():
     b = request.form.get('Body','')
     phone = request.form.get('From','')
     wit = requests.get('https://api.wit.ai/message?v=20140905&q=%s' % b, headers={'Authorization':'Bearer L3VB34V6YTDFO4BRXNDQNAYMVOOF4BHB'}).text
+    intent = json.loads(wit)['outcomes'][0]['intent']
+    if intent == 'get_status':
+        m = get_status(wit, phone)
+    elif intent == 'remind':
+        entities = json.loads(wit)['outcomes'][0]['entities']
+        date = datetime.strptime(entities['time'][0]['value']['from'],"%Y-%m-%dT%H:%M:%S.Z")
+        text = entities['message']
+        m = create_reminder(date, text, phone)
+    else:
+        m = "Hmm? Try again please :("
+
     m = get_status(wit, phone)
     # Send to wit.ai for processing
     resp = twilio.twiml.Response()
