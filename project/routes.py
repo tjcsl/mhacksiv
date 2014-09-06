dd288e9eb997b6fdb00a5462bf64d7a49ca87ec4
csssuf <csssuf@csssuf.net>
2014-09-06 01:54:44 -0400

    Add flask framework, twilio endpoints

---
+++ project/routes.py
@@ -0,0 +1,12 @@
+from project import app
+
+def add_url_routes(routes_tuple):
+    for route, view_function in routes_tuple:
+        app.add_url_rule(route, view_function.__name__, view_function,
+                methods=["GET", "POST"])
+
+add_url_routes((
+    ('/internal/call', twilio.call),
+    ('/internal/text', twilio.text),
+    ('/internal/rec', twilio.rec)
+))
---
+++ project/views/__init__.py
@@ -0,0 +1,5 @@
+from ..views import (
+        twilio
+)
+
+from .. import app
