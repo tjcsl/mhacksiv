8899266b6c025dfed891820613c47f12a4b0ec8a
fwilson <fwilson@fwilson.me>
2014-09-06 01:37:57 -0400

    add get_status

--- status.py
+++ status.py
@@ -1,2 +1,13 @@
+import json
+import requests
+def get_alias(phone, shortname):
+    return shortname
+
 def get_status(wit_json, phone):
-    return "Nothing to see here."
+    wit_json = json.loads(wit_json)
+    entities = wit_json["outcomes"]["entities"]
+    server = entities["server"][0]["value"]
+    status_entity = entities["status_item"][0]["value"]
+    server = get_alias(phone, server)
+    resp = requests.get("http://%s:5000/%s/" % (server, status_entity)).json()
+    return resp["format_string"] % resp["value"]
