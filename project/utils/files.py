9b8e6f7a809a4907aa2bce82ab72d7151a8ce780
fwilson <fwilson@fwilson.me>
2014-09-06 18:47:41 -0400

    fwilson

--- project/utils/files.py
+++ project/utils/files.py
@@ -1,3 +1,5 @@
 import urllib
-def find(server, query):
-    return "http://%s:5000%s" % (server, requests.get("http://%s:5000/matchfile/%s/" % (server, urllib.urlencode(query))).json()["matches"][0])
+import requests
+from project.utils.status import get_alias
+def find(server, query, phone):
+    return "http://%s:5000%s" % (get_alias(phone, server), requests.get("http://%s:5000/matchfile/%s/" % (get_alias(phone, server), urllib.urlencode(query))).json()["matches"][0])
