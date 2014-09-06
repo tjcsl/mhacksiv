c419053c63068269332c4accc63b4016b9325b3f
Vacation9 <samuel.damashek@gmail.com>
2014-09-05 22:43:43 -0700

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def text():
     b = request.form.get('Body','')
     phone = request.form.get('From','')
     wit = requests.get('https://api.wit.ai/message?v=20140905&q=%s' % b, headers={'Authorization':'Bearer L3VB34V6YTDFO4BRXNDQNAYMVOOF4BHB'}).text
-    m = get_status(b, phone)
+    m = get_status(wit, phone)
     # Send to wit.ai for processing
     resp = twilio.twiml.Response()
     resp.message(m)
