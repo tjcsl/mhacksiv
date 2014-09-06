8e5246cf9b95294c56d2ee76bcf25975de144c3b
fwilson <fwilson@fwilson.me>
2014-09-06 11:08:24 -0400

    debug status

--- project/utils/status.py
+++ project/utils/status.py
@@ -4,10 +4,13 @@ from project.models import Phone, Alias
 def get_alias(phone, shortname):
     phon = Phone.query.filter(Phone.phone_number == phone).first()
     if phon is None:
+        print("unknown phone -- using %s" % shortname)
         return shortname
     alis = Alias.query.filter(Alias.uid == phon.uid and Alias._from == shortname).first()
     if alis is None:
+        print("no alias found -- using %s" % shortname)
         return shortname
+    print("alias found! %s" % repr(alis))
     return alis.to
 
 def get_status(wit_json, phone):
