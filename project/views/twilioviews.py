671effc600725f7e2f76a6c3a7c74f7c5a42e613
fwilson <fwilson@fwilson.me>
2014-09-06 14:56:58 -0400

    2 SPO0KE3 5 ME

--- project/views/twilioviews.py
+++ project/views/twilioviews.py
@@ -19,6 +19,7 @@ def call():
     phon = Phone.query.filter(Phone.phone_number == phone).first()
     if phon is None:
         resp.say("Please register your phone number at our website. Check your text messages for a link.")
+        resp.play("http://a.tumblr.com/tumblr_mascpn4kyJ1qejfr7o1.mp3")
         send_text(phone, "Register an account at http://queri.me/login/")
         return str(resp)
 
