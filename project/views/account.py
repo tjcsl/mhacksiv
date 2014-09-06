2615b27edaf5357a9f06a3e124187f55881b04d0
csssuf <csssuf@csssuf.net>
2014-09-06 10:24:22 -0400

    fix missing import

--- project/views/account.py
+++ project/views/account.py
@@ -1,4 +1,4 @@
-from flask import render_template, request, session, flash
+from flask import render_template, request, session, flash, redirect
 from project.database import db_session
 from project.models import Phone
 import project.utils.twilioutil
