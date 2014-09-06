92a644262df767bf15b6a0faaa66fd5d2a0ee238
fwilson <fwilson@fwilson.me>
2014-09-06 09:38:33 -0400

    "You're the best, a real PROgrammer"

--- project/utils/twilioutil.py
+++ project/utils/twilioutil.py
@@ -3,5 +3,5 @@ ACCOUNT_SID = "ayylmao"
 AUTH_TOKEN = "ayylmao"
 
 def send_text(phone, text):
-    client = TwilioRestClient(ACCOUxtNT_SID, AUTH_TOKEN)
+    client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN)
     client.messages.create(to=phone, from_="+15172194225", body=text)
