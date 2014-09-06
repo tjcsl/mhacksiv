094640bdfaa0ed2e885eb2dab668ad27c5205e31
csssuf <csssuf@csssuf.net>
2014-09-06 02:02:09 -0400

    Fix view import

--- project/routes.py
+++ project/routes.py
@@ -1,5 +1,7 @@
 from project import app
 
+from project.views import twilio
+
 def add_url_routes(routes_tuple):
     for route, view_function in routes_tuple:
         app.add_url_rule(route, view_function.__name__, view_function,
